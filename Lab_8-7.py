8e004d92007845c83fa293b41c879920da2efba7
JM <p23jmarotta@fairfieldprep.org>
2023-12-07 12:05:46 -0500

    8-7

--- Lab_8-7.py
+++ Lab_8-7.py
@@ -2,7 +2,6 @@ def print_possessive_names(data):
     for row in data:
         name = row[0]
 
-        # Check if the name ends with 's' and adjust the possessive form accordingly
         if name[-1] == 's':
             possessive_name = name + "'"
         else:
